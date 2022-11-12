dc895d204f47185ec49559363f3316a8f070646e
FlavioAcuna <flaviofeat@gmail.com>
2022-11-12 06:51:23 -0300

    Changes to be committed: modified:   index.html modified:   main.js modified:   style.css

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
+const API_URL = "http://localhost:4000";
 window.onload = function () {
-  const API_URL = "http://localhost:4000";
+  
 
   const cargarProductos = document.querySelector("#div-productos");
 
@@ -10,21 +11,18 @@ window.onload = function () {
   const mostrarProductos = (productos) => {
     let productos_bs = "";
     for (let i = 0; i < productos.length; i++) {
-      productos_bs += `<div class="col-md-3 col-sm-6 mb-5">
-      <div class="product-grid">
+      productos_bs += `<div class="col-lg-3 col-md-4 col-sm-6 mb-4">
+      <div class="card h-100">
         <div class="product-image">
             <img
               class="pic-1"
               src="${productos[i].url_image}"
             />
-          <span class="product-new-label">Oferta</span>
-          <span class="product-discount-label">${productos[i].discount}%</span>
         </div>
   
-        <div class="product-content">
-          <h3 class="title">${productos[i].name}</h3>
-          <div class="price">$${productos[i].price}</div>
-          <a class="add-to-cart" href="">Añadir al carro</a>
+        <div class="card-body">
+          <h3 class="card-title">${productos[i].name}</h3>
+          <div class="card-text">$${productos[i].price}</div>
         </div>
       </div>
     </div>
@@ -32,7 +30,7 @@ window.onload = function () {
       cargarProductos.innerHTML = `${productos_bs}`;
     }
 
-    const cargarCategorias = document.querySelector("#dropdown_Categoria");
+    const cargarCategorias = document.querySelector("#cargarCategoria");
     fetch(`${API_URL}/categorias`)
       .then((res) => res.json())
       .then((categorias) => mostrarCategorias(categorias));
@@ -40,12 +38,74 @@ window.onload = function () {
     const mostrarCategorias = (categorias) => {
       let categorias_bs = "";
       for (let i = 0; i < categorias.length; i++) {
-        categorias_bs += `<li>
-      <a class="dropdown-item"  href="#">${categorias[i].name}</a> 
-      </li>`;
+        categorias_bs += `<option value="${categorias[i].id}">
+      ${categorias[i].name}
+      </option>`;
       }
-      cargarCategorias.innerHTML = `${categorias_bs}`;
+      cargarCategorias.innerHTML = `<option selected disabled>Categorias</option>${categorias_bs}`;
     };
   };
 };
+function cargarProduct_category() {
+  
+  const cargarProductos = document.querySelector("#div-productos");
+  var idCat = document.getElementById("cargarCategoria").value;
+  fetch(`${API_URL}/categorias/${idCat}`)
+    .then((res) => res.json())
+    .then((productos) => mostrarProductos(productos));
 
+  const mostrarProductos = (productos) => {
+    let productos_bs = "";
+    for (let i = 0; i < productos.length; i++) {
+      productos_bs += `<div class="col-lg-3 col-md-4 col-sm-6 mb-4">
+      <div class="card h-100">
+        <div class="product-image">
+            <img
+              class="pic-1"
+              src="${productos[i].url_image}"
+            />
+        </div>
+  
+        <div class="card-body">
+          <h3 class="card-title">${productos[i].name}</h3>
+          <div class="card-text">$${productos[i].price}</div>
+        </div>
+      </div>
+    </div>
+    `;
+      cargarProductos.innerHTML = `${productos_bs}`;
+    }
+  };
+}
+
+function findProductos() {
+  
+  const cargarProductos = document.querySelector("#div-productos");
+  const findName = document.getElementById("buscar_bar").value;
+  fetch(`${API_URL}/productos/${findName}`)
+    .then((res) => res.json())
+    .then((productos) => mostrarProductos(productos));
+
+  const mostrarProductos = (productos) => {
+    let productos_bs = "";
+    for (let i = 0; i < productos.length; i++) {
+      productos_bs += `<div class="col-lg-3 col-md-4 col-sm-6 mb-4">
+      <div class="card h-100">
+        <div class="product-image">
+            <img
+              class="pic-1"
+              src="${productos[i].url_image}"
+            />
+        </div>
+  
+        <div class="card-body">
+          <h3 class="card-title">${productos[i].name}</h3>
+          <div class="card-text">$${productos[i].price}</div>
+        </div>
+      </div>
+    </div>
+    `;
+      cargarProductos.innerHTML = `${productos_bs}`;
+    }
+  };
+}
